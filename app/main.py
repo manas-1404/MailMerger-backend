acb570f93e77099e91e07bc836eec5f565d536ba
manas-1404 <manas.gowda04@gmail.com>
2025-06-14 23:41:11 +0200

    create tables after app startup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from app.db.dbConnection import engine
+from app.models.base_model import Base
+import app.models
 app = FastAPI()
 
 
@@ -11,3 +14,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.on_event("startup")
+async def db_create_tables():
+    Base.metadata.create_all(bind=engine)
---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.user_models import User
+from app.models.user_token_models import UserToken
+from app.models.email_models import Email
+from app.models.template_models import Template
