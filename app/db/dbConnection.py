7cd409e22739d1f841e0a0e052cf0291932d18cd
manas-1404 <manas.gowda04@gmail.com>
2025-06-16 22:45:46 +0200

    added db session dependency function

--- app/db/dbConnection.py
+++ app/db/dbConnection.py
@@ -5,3 +5,19 @@ from app.utils.config import settings
 engine = create_engine(settings.DB_CONNECTION_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db_session():
+    """
+    Dependency to get a database session.
+    This function can be used in FastAPI routes to get a session for database operations.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+
+    except Exception as e:
+        db.rollback()
+        raise e
+
+    finally:
+        db.close()
