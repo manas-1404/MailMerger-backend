de296d913d5719a8095280313158be303c2cdce6
manas-1404 <manas.gowda04@gmail.com>
2025-07-16 00:30:54 +0200

    added delete list of emails in queue api

--- app/routes/queue_routes.py
+++ app/routes/queue_routes.py
@@ -156,3 +156,52 @@ def send_queued_emails(email_ids: List[int] = Body(...),
         message=f"{len(email_ids)} emails added to background worker successfully.",
         data={"sent_emails": email_ids}
     )
+
+@queue_router.delete("/delete-queue-email")
+def delete_queue_email(email_ids: List[int] = Body(...),
+                                   jwt_payload: dict = Depends(authenticate_request),
+                                   db_connection: Session = Depends(get_db_session),
+                                   redis_connection: redis.Redis = Depends(get_redis_connection)):
+    """
+    Endpoint to delete emails from the queue.
+    """
+    user_id: str = jwt_payload.get("sub")
+
+    user: User | None = db_connection.query(User).filter(User.uid == user_id).first()
+
+    if not user:
+        return ResponseSchema(
+            success=False,
+            status_code=404,
+            message="User not found.",
+            data={}
+        )
+
+    redis_email_queue_key: str = f"email_queue:{user_id}"
+
+    email_queue = redis_connection.lrange(redis_email_queue_key, 0, -1)
+
+    redis_pipeline = redis_connection.pipeline()
+    redis_pipeline.delete(redis_email_queue_key)
+    remaining_queue_length = 0
+
+    for email in email_queue:
+        email_data = json.loads(email)
+
+        if email_data.get("eid") not in email_ids:
+            redis_pipeline.rpush(redis_email_queue_key, email)
+            remaining_queue_length += 1
+            continue
+
+    db_connection.query(Email).filter(Email.eid.in_(email_ids)).delete(synchronize_session=False)
+    db_connection.commit()
+
+    redis_pipeline.expire(redis_email_queue_key, 90*60)
+    redis_pipeline.execute()
+
+    return ResponseSchema(
+        success=True,
+        status_code=200,
+        message=f"{len(email_ids)} emails deleted from the queue successfully.",
+        data={"deleted_emails_ids": email_ids, "remaining_queue_length": remaining_queue_length}
+    )
