da62c7ca3e5118d53b86b93ebef8e04d2a51d484
manas-1404 <manas.gowda04@gmail.com>
2025-07-16 01:42:40 +0200

    implemented batched db call in celery worker

--- app/tasks/celery_tasks.py
+++ app/tasks/celery_tasks.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 from typing import List
 
+from sqlalchemy import text
 from sqlalchemy.orm import joinedload
 
 from app.celery_worker import celery_app
@@ -26,7 +27,9 @@ def send_emails_from_user_queue(user_id: str, email_ids: List[int]):
     redis_queue_key = f"email_queue:{user_id}"
     redis_failed_queue_key = f"failed_email_queue:{user_id}"
 
-    print(f"Processing emails for user_id: {user_id} with email_ids: {email_ids}")
+    updated_send_at_records = {}
+    updated_google_message_id_records = {}
+    new_db_records = []
 
     try:
 
@@ -34,37 +37,36 @@ def send_emails_from_user_queue(user_id: str, email_ids: List[int]):
 
         email_queue = redis_connection.lrange(redis_queue_key, 0, -1)
 
-        redis_connection.delete(redis_queue_key)
+        redis_pipeline = redis_connection.pipeline()
 
-        skipped_emails = []
+        redis_pipeline.delete(redis_queue_key)
 
         for email_json in email_queue:
 
             email_data = json.loads(email_json)
 
-            print(f"Processing email data: \n{email_data}")
-
             if email_data.get("eid") not in email_ids:
                 #If the email ID is not in the provided list, skip processing this email
-                skipped_emails.append(email_json)
+                redis_pipeline.rpush(redis_queue_key, email_json)
                 continue
 
             email_object = EmailSchema.model_validate(email_data)
 
-            service_response = gmail_send_message(email_object=email_object, google_access_token=user.user_tokens[0].access_token, from_email=user.email, user_token=user.user_tokens[0], db_connection=db_connection)
+            try:
+                service_response = gmail_send_message(email_object=email_object, google_access_token=user.user_tokens[0].access_token, from_email=user.email, user_token=user.user_tokens[0], db_connection=db_connection)
+            except Exception as e:
+                print(f"Error sending email: {e}")
+                email_data["retry_count"] = email_data.get("retry_count", 0) + 1
+                redis_pipeline.rpush(redis_failed_queue_key, json.dumps(email_data))
 
             if service_response:
                 if email_object.eid:
                     #update the status of the email in db
-                    db_connection.query(Email).filter(Email.eid == email_object.eid).update({
-                        Email.is_sent: True,
-                        Email.google_message_id: service_response.get('id') ,
-                        Email.send_at: datetime.utcnow()
-                    })
+                    updated_send_at_records[email_object.eid] = datetime.utcnow()
+                    updated_google_message_id_records[email_object.eid] = service_response.get('id')
 
                 else:
                     #this is a new email directly from redis
-
                     new_email = Email(
                         uid=user_id,
                         google_message_id=service_response.get('id') ,
@@ -76,16 +78,46 @@ def send_emails_from_user_queue(user_id: str, email_ids: List[int]):
                         bcc_email=email_object.bcc_email,
                         send_at=datetime.utcnow()
                     )
-
-                    db_connection.add(new_email)
+                    new_db_records.append(new_email)
 
             else:
                 email_data["retry_count"] = email_data.get("retry_count", 0) + 1
-                redis_connection.rpush(redis_failed_queue_key, json.dumps(email_data))
+                redis_pipeline.rpush(redis_failed_queue_key, json.dumps(email_data))
 
-        if skipped_emails:
-            redis_connection.rpush(redis_queue_key, *skipped_emails)
-            redis_connection.expire(redis_queue_key, 90 * 60)
+        redis_pipeline.expire(redis_queue_key, 90 * 60)
+        redis_pipeline.expire(redis_failed_queue_key, 90 * 60)
+
+        redis_pipeline.execute()
+
+        if new_db_records:
+            db_connection.bulk_save_objects(new_db_records)
+
+        if updated_send_at_records and updated_google_message_id_records:
+
+            eids = list(updated_send_at_records.keys())
+
+            send_at_case = "CASE\n"
+            google_message_id_case = "CASE\n"
+
+            for eid in eids:
+                send_at = updated_send_at_records[eid]
+                google_message_id = updated_google_message_id_records[eid]
+                send_at_case += f"  WHEN eid = {eid} THEN '{send_at.isoformat()}'::timestamp\n"
+                google_message_id_case += f"  WHEN eid = {eid} THEN '{google_message_id}'\n"
+
+            send_at_case += "END"
+            google_message_id_case += "END"
+
+            sql_query = f"""
+                    UPDATE emails
+                    SET 
+                        is_sent = TRUE,
+                        send_at = {send_at_case},
+                        google_message_id = {google_message_id_case}
+                    WHERE eid IN ({','.join(map(str, eids))});
+                """
+
+            db_connection.execute(text(sql_query))
 
         db_connection.commit()
 
