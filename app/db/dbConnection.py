ed7fc3363303235b19e84b4467c0ff78f87e6094
manas-1404 <manas.gowda04@gmail.com>
2025-08-24 21:24:38 -0700

    updated dbconnection files

--- app/db/dbConnection.py
+++ app/db/dbConnection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.utils.config import settings
 
-engine = create_engine(settings.DB_CONNECTION_URL, pool_pre_ping=True)
+engine = create_engine(settings.NEON_DB_CONNECTION_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
